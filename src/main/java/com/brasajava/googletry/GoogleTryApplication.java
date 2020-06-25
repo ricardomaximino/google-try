6f47f289820b0e8240cf2f3f3f081f764617b98b
Goncalves De Moraes Ricardo Maximino <rmaximin@intrasoft-intl.com>
2020-06-25 17:23:39 +0200

    Initial commit

---
+++ src/main/java/com/brasajava/googletry/GoogleTryApplication.java
@@ -0,0 +1,39 @@
+package com.brasajava.googletry;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import java.time.LocalDate;
+
+@SpringBootApplication
+public class GoogleTryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleTryApplication.class, args);
+	}
+
+}
+@RepositoryRestResource(collectionResourceRel = "person", path = "person")
+interface PersonRepository extends JpaRepository<Person, Long>{}
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+class Person{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private int age;
+
+}
